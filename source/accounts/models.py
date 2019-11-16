616548e61a25b693b9c69d41d838d077609b63c3
Айсулу <aisuluueco2009@yandex.ru>
2019-11-16 15:11:14 +0600

    Сделала вход и выход для пользователя.

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name='user_profiles', on_delete=models.CASCADE, verbose_name='Пользователь')
+    first_name = models.CharField(max_length=50, verbose_name='Имя')
+    last_name = models.CharField(max_length=100, verbose_name='Фамилия')
+    avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Фото пользователя')
+    email = models.EmailField(max_length=200, verbose_name='Email')
+
+    def __str__(self):
+        return self.user.get_full_name() + "'s Profile"
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from accounts.views import register_view
 
 app_name = 'accounts'
 
 urlpatterns = [
+    path('accounts/login/', LoginView.as_view(), name='login'),
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
     path('register/', register_view, name='register'),
 
 ]
