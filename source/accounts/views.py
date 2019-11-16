3a35b8f6fecd9023f31c5a9c3ac8b7cffb1b0e8e
Айсулу <aisuluueco2009@yandex.ru>
2019-11-16 16:49:15 +0600

    При просмотре услуги сделала картинку-заглушку.

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,25 +1,12 @@
 from django.contrib.auth import login
 from django.contrib.auth.mixins import UserPassesTestMixin
-
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.views.generic import DetailView, UpdateView
-
 from accounts.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from accounts.models import Profile
 
-
-# def register_view(request, *args, **kwargs):
-#     if request.method == 'POST':
-#         form = UserCreationForm(data=request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             login(request, user)
-#             return redirect('webapp:index')
-#     else:
-#         form = UserCreationForm()
-#     return render(request, 'create_user.html', context={'form': form})
 
 def register_view(request):
     if request.method == 'POST':
