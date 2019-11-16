586622cba7025f483dd3cc989b4726a019d2bd26
Айсулу <aisuluueco2009@yandex.ru>
2019-11-16 12:23:02 +0600

    Создала модель 'Services', создала миграцию и применила ее.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+CATEGORY_CHOICES = (
+    ('other', 'Другое'),
+    ('individuals', 'Физические лица'),
+    ('legal entities', 'Юридические лица'),
+    ('сard service', 'Карточное обслуживание'),
+    ('internet banking', 'Интернет банкинг'),
+)
+
+
+class Services(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Услуги')
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default=CATEGORY_CHOICES[0][0],
+                                verbose_name='Категория')
+    photo = models.ImageField(upload_to='product_images', null=True, blank=True, verbose_name='Фото')
+    description = models.TextField(max_length=1000, null=True, blank=True, verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Услуга'
+        verbose_name_plural = 'Услуги'
+
+
+
